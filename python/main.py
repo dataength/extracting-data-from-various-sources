eba071192d7e86cb77ca8392a3cc9fe642d699a4
zkan <zkan.cs@gmail.com>
2021-08-29 12:15:37 +0700

    Extract data from Mongo :bear:

---
+++ python/main.py
@@ -0,0 +1,24 @@
+import configparser
+
+import pymongo
+
+
+if __name__ == "__main__":
+    parser = configparser.ConfigParser()
+    parser.read("pipeline.conf")
+
+    dbname = parser.get("mongo_config", "database")
+    collection = parser.get("mongo_config", "collection")
+    username = parser.get("mongo_config", "username")
+    password = parser.get("mongo_config", "password")
+    hostname = parser.get("mongo_config", "hostname")
+
+    dsn = f"mongodb+srv://{username}:{password}@{hostname}/{dbname}?retryWrites=true&w=majority"
+    client = pymongo.MongoClient(dsn)
+
+    db = client["sample_airbnb"]
+    collection = db["listingsAndReviews"]
+    query = {}
+    docs = collection.find(query, batch_size=1000)
+    for each in docs:
+        print(each)
